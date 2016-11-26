e706a56fff7d7ded30cef0971dccca589cbbb301
jakehirzel <hirzel@hotmail.com>
2016-11-26 11:15:44 -0500

    Added code for RATE US linking; updated background color to match icon.

--- Roll/InfoViewController.swift
+++ Roll/InfoViewController.swift
@@ -19,6 +19,39 @@ class InfoViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    // MARK: Convenience
+    
+    func rateUs() {
+        
+        // Set the App ID
+        let appID = "1179036062"
+        
+        // Check for valid URL & open if valid
+        if let checkURL = URL(string: "http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=\(appID)&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8") {
+            
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(checkURL, options: [:], completionHandler: { (success) in
+                    print("Open \(checkURL): \(success)")
+                })
+            }
+            else {
+                UIApplication.shared.openURL(checkURL)
+                print("Open \(checkURL) successful")
+            }
+        }
+        else {
+            print("Invalid URL")
+        }
+        
+    }
+    
     // MARK: Actions
+    
+    @IBAction func rateButtonTapped(_ sender: UIButton) {
+        
+        rateUs()
+        
+    }
+    
     
 }
