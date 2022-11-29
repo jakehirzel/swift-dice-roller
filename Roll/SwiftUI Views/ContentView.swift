78e823a86f8a12b11452304fa2922e2c1a07c000
jakehirzel <restingdeer@gmail.com>
2022-11-29 14:00:26 -0500

    Continued to rough in ContentView; added custom color to Assets.

--- Roll/SwiftUI Views/ContentView.swift
+++ Roll/SwiftUI Views/ContentView.swift
@@ -11,30 +11,46 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State var dicePickerChoice = "Six-Sided"
+    @State var dieTypeChoice = "Six-Sided"
+    @State var dieQuantityChoice = "1 Die"
+    @State var rollTypeChoice = "Standard"
 
     var body: some View {
-        VStack {
-            HStack(alignment: .center, spacing: 10) {
-                Text("Die 1")
-                Text("Die 2")
+        ZStack {
+            Color("RollEmBlue").ignoresSafeArea()
+            VStack {
+                HStack(alignment: .center, spacing: 10) {
+                    Text("Die 1")
+                    Text("Die 2")
+                }
+                Button("Roll", action: { print("Roll Button Pressed.") } )
+                Picker("Die Type:", selection: $dieTypeChoice) {
+                    Text("Four-Sided")
+                    Text("Six-Sided")
+                    Text("Eight-Sided")
+                    Text("Ten-Sided")
+                    Text("Twelve-Sided")
+                    Text("Twenty-Sided")
+                    Text("A-Z")
+                }
+                HStack(alignment: .center) {
+                    Picker("Die Quantity", selection: $dieQuantityChoice) {
+                        Text("1 Die")
+                        Text("2 Dice")
+                    }
+                    .pickerStyle(.segmented)
+                    Picker("RollType", selection: $rollTypeChoice) {
+                        Text("Standard")
+                        Text("Shuffled")
+                    }
+                    .pickerStyle(.segmented)
+                    Button(action: { print("Roll Button Pressed.") } ) {
+                        Image(systemName: "info.circle")
+                    }
+                }
+                .padding()
             }
-            Button("Roll", action: { print("Roll Button Pressed.") } )
-            Picker("Die Type:", selection: $dicePickerChoice) {
-                Text("Four-Sided")
-                Text("Six-Sided")
-                Text("Eight-Sided")
-                Text("Ten-Sided")
-                Text("Twelve-Sided")
-                Text("Twenty-Sided")
-                Text("A-Z")
-            }
-            .pickerStyle(.wheel)
-            HStack(alignment: .center, spacing: 10) {
-                Text("Segmented Control 1")
-                Text("Segmented Control 2")
-                Text("Info Button")
-            }
+            .foregroundColor(.white)
         }
     }
 }
