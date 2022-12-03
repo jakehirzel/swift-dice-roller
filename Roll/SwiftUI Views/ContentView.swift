c23e032cade1143bf436e182ddcb8ab97c7d15d0
jakehirzel <restingdeer@gmail.com>
2022-12-03 14:35:25 -0500

    Roughing in Tab View.

--- Roll/SwiftUI Views/ContentView.swift
+++ Roll/SwiftUI Views/ContentView.swift
@@ -16,58 +16,79 @@ struct ContentView: View {
     @State var rollTypeChoice = "Standard"
 
     var body: some View {
-        ZStack {
-            Color("RollEmBlue").ignoresSafeArea()
-            VStack {
-                HStack(alignment: .center, spacing: 10) {
-                    Text("24")
-                        .frame(width: 100, height: 100)
-                        .font(.system(size: 70, weight: .black))
-                        .foregroundColor(Color("RollEmBlue"))
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 16)
-                            .foregroundColor(.white)
-                        )
-                    Text("2")
-                        .frame(width: 100, height: 100)
-                        .font(.system(size: 70, weight: .black))
-                        .foregroundColor(Color("RollEmBlue"))
-                        .padding()
-                        .background(
-                            RoundedRectangle(cornerRadius: 16)
-                            .foregroundColor(.white)
-                        )
-                }
-                Button("Roll", action: { print("Roll Button Pressed.") } )
-                Picker("Die Type:", selection: $dieTypeChoice) {
-                    Text("Four-Sided")
-                    Text("Six-Sided")
-                    Text("Eight-Sided")
-                    Text("Ten-Sided")
-                    Text("Twelve-Sided")
-                    Text("Twenty-Sided")
-                    Text("A-Z")
-                }
-                .pickerStyle(.wheel)
-                HStack(alignment: .center) {
-                    Picker("Die Quantity", selection: $dieQuantityChoice) {
-                        Text("1 Die")
-                        Text("2 Dice")
+        TabView {
+            ZStack {
+                Color("RollEmBlue")
+                    .ignoresSafeArea(.all, edges: .top)
+                VStack {
+                    HStack(alignment: .center, spacing: 10) {
+                        Text("24")
+                            .frame(width: 100, height: 100)
+                            .font(.system(size: 70, weight: .black))
+                            .foregroundColor(Color("RollEmBlue"))
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 16)
+                                .foregroundColor(.white)
+                            )
+                        Text("2")
+                            .frame(width: 100, height: 100)
+                            .font(.system(size: 70, weight: .black))
+                            .foregroundColor(Color("RollEmBlue"))
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 16)
+                                .foregroundColor(.white)
+                            )
                     }
-                    .pickerStyle(.segmented)
-                    Picker("RollType", selection: $rollTypeChoice) {
-                        Text("Standard")
-                        Text("Shuffled")
+                    Button("Roll", action: { print("Roll Button Pressed.") } )
+                    Picker("Die Type:", selection: $dieTypeChoice) {
+                        Text("Four-Sided")
+                        Text("Six-Sided")
+                        Text("Eight-Sided")
+                        Text("Ten-Sided")
+                        Text("Twelve-Sided")
+                        Text("Twenty-Sided")
+                        Text("A-Z")
                     }
-                    .pickerStyle(.segmented)
-                    Button(action: { print("Roll Button Pressed.") } ) {
-                        Image(systemName: "info.circle")
+                    .pickerStyle(.wheel)
+                    HStack(alignment: .center) {
+                        Picker("Die Quantity", selection: $dieQuantityChoice) {
+                            Text("1 Die")
+                            Text("2 Dice")
+                        }
+                        .pickerStyle(.segmented)
+                        Picker("RollType", selection: $rollTypeChoice) {
+                            Text("Standard")
+                            Text("Shuffled")
+                        }
+                        .pickerStyle(.segmented)
+                        Button(action: { print("Roll Button Pressed.") } ) {
+                            Image(systemName: "info.circle")
+                        }
                     }
+                    .padding()
                 }
-                .padding()
+                .foregroundColor(.white)
             }
-            .foregroundColor(.white)
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+            Text("Roll")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .tabItem {
+                    Image(systemName: "dice")
+                    Text("Roll")
+                }
+            Text("Info")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .tabItem {
+                    Image(systemName: "info.circle.fill")
+                    Text("Info")
+                }
         }
     }
 }
